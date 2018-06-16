2ec54c1599046bcffb31182294be62c567c4b9dc
7-Zip <mattkorn129@gmail.com>
2018-06-15 20:04:13 -0400

    Completed todo creation functionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,14 +1,25 @@
 // Check off specific todos by clicking
-$("li").click(function() {
+$("ul").on("click", "li", function() {
 	$(this).toggleClass("completed");
 });
 
 // Click on X to delete todo
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
 	// Fade and remove entire li
 	$(this).parent().fadeOut(500, function() {
 		$(this).remove();
 	});
 	// Stop click event from bubbling to parents
 	event.stopPropagation();
-})
+});
+
+$("input[type='text']").keypress(function(event) {
+	if(event.which === 13) {
+		// Get text for new todo from input
+		let todoText = $(this).val();
+		// Clear input text
+		$(this).val("");
+		// Create a new li and add to ul
+		$("ul").append("<li><span>X</span> " + todoText + "</li>")
+	}
+});
